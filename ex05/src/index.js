c3aa350cf7d8d1cf6ca421ee6662313adcda4908
amir-pasalic <amirpasalic144@gmail.com>
2021-03-25 21:34:53 +0100

    Assignment done !

---
+++ ex05/src/index.js
@@ -0,0 +1,27 @@
+let arrKey = [];
+let arrValue = [];
+let shoppingList = new Map([
+  ["Banana", 3],
+  ["Pineapple", 5],
+  ["Pear", 2],
+  ["Carrot", 10],
+  ["Apple", 1.5],
+]);
+
+for (let col of shoppingList.entries()) {
+  arrKey.push(col[0]);
+}
+for (let i = 0; i < arrKey.length; i++) {
+  console.log("groceries:  " + arrKey[i]);
+}
+for (let col of shoppingList.entries()) {
+  arrValue.push(col[1]);
+}
+for (let i = 0; i < arrValue.length; i++) {
+  console.log("amount:  " + arrValue[i]);
+}
+for (let col of shoppingList.entries()) {
+  console.log(col);
+}
+
+module.exports = { shoppingList, arrKey, arrValue };
